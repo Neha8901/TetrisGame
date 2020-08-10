c87cb6819a023a096312ab9e50d887a946eeacc3
Nehagan08 <nehagandhmal123@gmail.com>
2020-08-10 13:42:36 +0530

    Terminoes creation & Random selection of termino

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,51 @@
-document.addEventListener("DocumentContentLoaded", () => {
+document.addEventListener('DocumentContentLoaded', () => {
 
+    const grid = document.querySelector('.grid');
     const width = 10;
     const height = 20;
-    
+
+    //The terminoes
+
+    const lTetromino = [
+        [1, width + 1, width * 2 + 1, 2],
+        [width, width + 1, width + 2, width * 2 + 2],
+        [1, width + 1, width * 2 + 1, width * 2],
+        [width, width * 2, width * 2 + 1, width * 2 + 2]
+    ];
+
+    const zTetromino = [
+        [1, width + 1, width * 2 + 1, 2],
+        [width, width + 1, width + 2, width * 2 + 2],
+        [1, width + 1, width * 2 + 1, width * 2],
+        [width, width * 2, width * 2 + 1, width * 2 + 2]
+    ];
+
+    const tTetromino = [
+        [1, width + 1, width * 2 + 1, 2],
+        [width, width + 1, width + 2, width * 2 + 2],
+        [1, width + 1, width * 2 + 1, width * 2],
+        [width, width * 2, width * 2 + 1, width * 2 + 2]
+    ];
+
+    const oTetromino = [
+        [1, width + 1, width * 2 + 1, 2],
+        [width, width + 1, width + 2, width * 2 + 2],
+        [1, width + 1, width * 2 + 1, width * 2],
+        [width, width * 2, width * 2 + 1, width * 2 + 2]
+    ];
+
+    const iTetromino = [
+        [1, width + 1, width * 2 + 1, 2],
+        [width, width + 1, width + 2, width * 2 + 2],
+        [1, width + 1, width * 2 + 1, width * 2],
+        [width, width * 2, width * 2 + 1, width * 2 + 2]
+    ];
+
+    const theTerminoes = [lTetromino, zTetromino, tTetromino, oTetromino, iTetromino];
+
+    //Randomly selecting terminals
+
+    let random = Math.floor(Math.random() * theTerminoes.length);
+    let currentRotation = 0;
+    let current = theTerminoes[random];
 });
