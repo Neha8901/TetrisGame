6213eb38af3a3c87d71a2d1a0974f900396fb6d9
Nehagan08 <nehagandhmal123@gmail.com>
2020-08-10 15:44:11 +0530

    Added function to draw, undraw, move, rotate functions for terminoes

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,10 @@
 document.addEventListener('DocumentContentLoaded', () => {
 
     const grid = document.querySelector('.grid');
+    let squares = Array.from(grid.querySelectorAll('div'));
     const width = 10;
     const height = 20;
+    let currentPosition = 4;
 
     //The terminoes
 
@@ -48,4 +50,72 @@ document.addEventListener('DocumentContentLoaded', () => {
     let random = Math.floor(Math.random() * theTerminoes.length);
     let currentRotation = 0;
     let current = theTerminoes[random];
+
+    //Function for drawing shapes
+
+    function draw() {
+        current.forEach(index => {
+            squares[currentPosition + index].classList.add('block');
+        });
+    }
+
+    //Function for undrawing shapes
+
+    function undraw() {
+        current.forEach(index => {
+            squares[currentPosition + index].classList.remove('block');
+        });
+    }
+
+    //Function to move the shape down
+
+    function moveDown() {
+        undraw();
+        currentPosition += width;
+        draw();
+        freeze();
+    }
+
+    //Function to move the shape to right side
+
+    function moveRight() {
+        undraw();
+        const isAtRightEdge = current.some(index => (currentPosition + index) % width === width - 1); 
+
+        if (!isAtRightEdge) {
+            currentPosition += 1;
+        }
+        if (current.some(index => squares[currentPosition + index].classList.contains('block2'))) {
+            currentPosition -= 1;
+        }
+        draw();
+    }
+
+    //Function to move the shape to left side
+
+    function moveLeft() {
+        undraw();
+        const isAtLeftEdge = current.some(index => (currentPosition + index) % width === 0); 
+
+        if (!isAtLeftEdge) {
+            currentPosition -= 1;
+        }
+        if (current.some(index => squares[currentPosition + index].classList.contains('block2'))) {
+            currentPosition += 1;
+        }
+        draw();
+    }
+
+    //Function for rotating terminal
+
+    function rotate() {
+        undraw();
+        currentRotation ++;
+
+        if (currentRotation === current.length) {
+            currentRotation = 0;
+        }
+        current = theTerminoes[random][currentRotation];
+        draw();
+    }
 });
